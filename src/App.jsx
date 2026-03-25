1cf1775a4f794a442fdfb3a0d45f9ee0e7a74fd4
Ankon 190 <ankon190@gmail.com>
2026-03-26 00:22:53 +0600

    navbar design completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
 
   return (
-    <div>
-      <h1 className="text-red-500 text-2xl text-center">Hello, Vite!</h1>
-      <button className="btn btn-primary">Click me</button>
+    <div className="max-w-10/12 m-auto">
+      <Navbar />
     </div>
+ 
   )
     
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+const Navbar = () => {
+    return (
+        <div className="navbar bg-base-100 shadow-sm">
+            <div className="flex-1">
+                    <img src="/src/assets/logo.png" alt="" />
+            </div>
+            <div className="flex gap-10 justify-center items-center">
+                <p className="text-[#494747]">Home</p>
+                <p className="text-[#494747]">Fixture</p>
+                <p className="text-[#494747]">Teams</p>
+                <p className="text-[#494747]">Schedules</p>
+                <button className="btn btn-ghost border-gray-200 rounded-lg">
+                    <span className=" font-bold">0 Coin</span>
+                    <img src="/src/assets/Currency.png" alt="" />
+                </button>
+            </div>
+        </div>
+    );
+}
+export default Navbar;
