88333f72623c35a9018119495a7b05a7e8c27a5c
Ankon 190 <ankon190@gmail.com>
2026-03-25 22:30:48 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className="text-red-500 text-2xl text-center">Hello, Vite!</h1>
+      <button className="btn btn-primary">Click me</button>
+    </div>
+  )
+    
+}
+
+export default App
