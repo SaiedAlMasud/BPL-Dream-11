32ac5fe13c45b25bb3628aaa2fdb18396ee09abc
Ankon 190 <ankon190@gmail.com>
2026-03-26 09:34:17 +0600

    Player Card dewsign completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Banner from './components/Homepage/Banner/Banner'
 import Navbar from './components/Navbar/Navbar'
+import Player from './components/players/Player'
 
 function App() {
 
@@ -9,6 +10,13 @@ function App() {
       <div className="max-w-11/12 m-auto">
         <Navbar />
         <Banner />
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 p-4">
+          <Player />
+          <Player />
+          <Player />
+        </div>
+        
+
       </div>
     </>
 
--- src/components/Homepage/Banner/Banner.jsx
+++ src/components/Homepage/Banner/Banner.jsx
@@ -2,7 +2,7 @@ RTCDTMFToneChangeEvent
 import React from 'react'
 const Banner = () => {
     return (
-        <div className="banner min-h-60vh bg-linear-to-r from-[#2b74c7] to-[#ccc4c4] justify-center items-center text-center flex flex-col gap-4 rounded-2xl">
+        <div className="banner min-h-60vh bg-linear-to-r from-[#3d88dd] to-[#ccc4c4] justify-center items-center text-center flex flex-col gap-4 rounded-2xl">
             <img className="mt-5" src="/src/assets/banner-main.png" alt="" />
             <h1 className="text-4xl font-bold text-[#130c0c]">Assemble Your Ultimate Dream 11 Cricket Team</h1>
             <p className="text-gray-600 mt-4">Beyond Boundaries Beyond Limits</p>
